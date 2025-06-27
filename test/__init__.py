03a9e9c24b5ce3193c35697cc57adebf42de2f39
ke4920 <jonas.dressner@kit.edu>
2025-06-27 22:03:13 +0200

    change format

--- test/__init__.py
+++ test/__init__.py
@@ -1,11 +1,12 @@
 # from .test_database_manager import AssasDatabaseManagerTest
 from .test_database_handler import AssasDatabaseHandlerTest
-#from .test_odessa_netcdf4_converter import AssasOdessaNetCDF4ConverterTest
+
+# from .test_odessa_netcdf4_converter import AssasOdessaNetCDF4ConverterTest
 from .test_document_file import AssasDocumentFileTest
 
 __all__ = [
     # "AssasDatabaseManagerTest",
     "AssasDatabaseHandlerTest",
-    #"AssasOdessaNetCDF4ConverterTest",
+    # "AssasOdessaNetCDF4ConverterTest",
     "AssasDocumentFileTest",
 ]
--- test/pyodessa.py
+++ test/pyodessa.py
@@ -1,14 +1,18 @@
 class R1(list):
     """Dummy R1 class, behaves like a list."""
+
     pass
+
 
 def get_saving_times(input_path):
     """Return a dummy list of time points."""
     return [0, 1, 2, 3]
 
+
 def restore(input_path, time_point):
     """Return a dummy OdessaBase object."""
     return OdessaBase()
+
 
 class OdessaBase:
     """Dummy OdessaBase object with minimal interface for testing."""
@@ -23,7 +27,18 @@ class OdessaBase:
     def get(self, name):
         # Return dummy data based on the name
         # For demonstration, return a list, float, or another OdessaBase as needed
-        if "MESH" in name or "FACE" in name or "PIPE" in name or "WALL" in name or "VOLUME" in name or "JUNCTION" in name or "CONNECTI" in name or "SOURCE" in name or "PUMP" in name or "VALVE" in name:
+        if (
+            "MESH" in name
+            or "FACE" in name
+            or "PIPE" in name
+            or "WALL" in name
+            or "VOLUME" in name
+            or "JUNCTION" in name
+            or "CONNECTI" in name
+            or "SOURCE" in name
+            or "PUMP" in name
+            or "VALVE" in name
+        ):
             return [1.0, 2.0]
         if "THER" in name or "GEOM" in name or "QMAV" in name:
             return [3.0]
