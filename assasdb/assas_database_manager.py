08acf3d706e17ee178075444f47d92a7a4be3919
ke4920 <ke4920@kit.edu>
2026-01-31 13:32:53 +0100

    correct modules

--- assasdb/assas_database_manager.py
+++ assasdb/assas_database_manager.py
@@ -1913,3 +1913,9 @@ except Exception as e:
 
         """
         logger.info("Link all files without user to the batch user.")
+
+        user_documents = self.database_handler.get_all_user_documents()
+        for user in user_documents:
+            roles = user.get("roles", [])
+            if "curator" in roles:
+                logger.info(f"curators: {user}")
